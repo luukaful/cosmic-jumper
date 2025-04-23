5a10da58265d4ac622fc91522d4cad34e80fb576
luukaful <luukaful@gmail.com>
2025-04-23 23:55:52 +0200

    Build v1.0.0

--- scripts/dist/dist_windows.bat
+++ scripts/dist/dist_windows.bat
@@ -1,13 +1,13 @@
 @echo off
 REM Step 1: Build the project into an .exe using pyinstaller
-pyinstaller --onefile --noconsole main.py
+pyinstaller --onefile --noconsole --name CosmicJumper --icon=assets\icon.ico main.py
 
 REM Step 2: Create a new folder for the build output
 set OUTPUT_DIR=build_output
 if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 
 REM Step 3: Move the .exe file into the new folder
-move dist\main.exe %OUTPUT_DIR%\
+move dist\CosmicJumper.exe %OUTPUT_DIR%\
 
 REM Step 4: Copy the assets folder into the new folder
 xcopy assets %OUTPUT_DIR%\assets /E /I /Y
@@ -15,7 +15,7 @@ xcopy assets %OUTPUT_DIR%\assets /E /I /Y
 REM Step 5: Clean up unnecessary build files
 rmdir /S /Q build
 rmdir /S /Q dist
-del main.spec
+del CosmicJumper.spec
 
-if exist %OUTPUT_DIR%\main.exe echo Build process completed successfully!
+if exist %OUTPUT_DIR%\CosmicJumper.exe echo Build process completed successfully!
 pause
